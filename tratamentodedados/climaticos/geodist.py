14594e9aab70c5fc19e011e2672f7ce7525991ba
Carlos Martinucci <carlos.martinucci@gmail.com>
2018-06-08 22:42:31 -0300

    Tratamento de dados do tipo climatico

--- tratamentodedados/climaticos/geodist.py
+++ tratamentodedados/climaticos/geodist.py
@@ -13,11 +13,15 @@
 from diblogeo import Geo
 
 
-# In[27]:
+# In[4]:
 
 
 def calcular_distancia_geoespacial(lat_long_ponto_1, lat_long_ponto_2):
-    if type(lat_long_ponto_1) != "tuple" or type(lat_long_ponto_2) != "tuple":
-        raise Exception("Varíaveis precisam ser do tipo tupla")
     return Geo(lat_long_ponto_1).distance(lat_long_ponto_2)
+
+
+# In[5]:
+
+
+calcular_distancia_geoespacial((1,2),(3,4))
 
--- tratamentodedados/climaticos/tratamento_base_climatica.py
+++ tratamentodedados/climaticos/tratamento_base_climatica.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[45]:
+# In[3]:
 
 
 import os
-from pandas import DataFrame, Series
+from pandas import DataFrame, Series, merge, read_csv
 
 
-# In[46]:
+# In[ ]:
 
 
 def recuperar_tuplas_de_bases_climaticas(caminho_arquivos):
@@ -53,23 +53,56 @@ def recuperar_tuplas_de_bases_climaticas(caminho_arquivos):
 
 
 
-# In[47]:
+# In[10]:
 
 
 def recuperar_data_frame_de_tuplas(caminho_arquivos):
     tuplas = recuperar_tuplas_de_bases_climaticas(caminho_arquivos)
-    data_frame = DataFrame(tuplas)
-    return data_frame
+    data_frame_clima = DataFrame(tuplas)
+    data_frame_direcao_vento = read_csv('resource/Codigo_Direcao_vento.csv',  encoding='utf-8', header=0, sep=";")
+    resultante = merge(data_frame_clima, data_frame_direcao_vento, how = 'left')
+    return resultante
 
 
-# In[48]:
+# In[8]:
 
 
-tuples = recuperar_data_frame_de_tuplas('resource/climahistorico/')
+def recuperar_wmos_lat_long(caminho_arquivos):
+    filelist = os.listdir(caminho_arquivos)
 
+    filepath = []
 
-# In[49]:
+    for i in filelist:
+        c = caminho_arquivos + i
+        filepath.append(c)
 
+    tuplas = []
 
-tuples
+    for nome_arquivo in filepath:
+        arquivo = open(nome_arquivo,mode = 'r')
+        linhas = arquivo.readlines()
+        estacao = 0
+        latitude = 0
+        longitude = 0
+        inicio_tabela = False
+
+        headers = []
+        for linha in linhas:
+            if not inicio_tabela:
+                if linha.find('Latitude') == 0:
+                    latitude = float(linha.split(':')[1].strip())
+                elif linha.find('Longitude') == 0:
+                    longitude = float(linha.split(':')[1].strip())
+                elif linha.find('Estacao;Data;Hora;TempBulboSeco;TempBulboUmido;UmidadeRelativa;PressaoAtmEstacao;DirecaoVento;VelocidadeVentoNebulosidade;') ==0:
+                    inicio_tabela = True
+            else:
+                tupla = dict()
+                colunas = linha.split(';')
+                tupla['Estacao'] = colunas[0]
+                tupla['Latitude'] = latitude
+                tupla['Longitude'] = longitude
+                tuplas.append(tupla)
+                break
+        
+    return DataFrame(tuplas)
 
