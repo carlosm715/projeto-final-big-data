686367369c2b224a3dfef93a61cd7529b2266cd4
Carlos Martinucci <carlos.martinucci@gmail.com>
2018-06-18 18:34:22 -0300

    V2 de datascience de vôos. Script de recuperação dos vôos na anac

---
+++ tratamentodedados/voos/recuperacao_dados_voo.sh
@@ -0,0 +1,34 @@
+################################
+# voos.sh
+#
+# Script para coletar os dados de voo da ANAC
+#
+################################
+
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/percentuais-de-atrasos-e-cancelamentos-2/2018/vra-01_2018.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/percentuais-de-atrasos-e-cancelamentos-2/2018/vra-02_2018.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_do_MS_012017.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_do_MS_022017.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_do_MES_032017.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_do_MS_042017.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_do_MES_052017.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_do_mes_062017bic.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/vra_do_mes_072017bic.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/vra_do_mes082017bic.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/vra_do_mes_092017_bic.xlsx
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_102017.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA_do_MS112017bic.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2017/VRA12_2017.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/vra_jan_2016_01.zip
+wget  http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/vra_fev_2016_02.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/vra_do_mes_032016.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/vra_do_mes_042016.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/VRA_do_Ms_052016.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/VRA_do_Ms_062016_BO.csv
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/VRA_do_Ms_072016_BIC.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/VRA_do_MS_082016_BO.zip
+wget http://www.anac.gov.br/assuntos/dados-e-estatisticas/base-historica-1/vra/2016/vra_do_mes_092016_bo.zip
+
+
+
+
